96d5120e7c9a1498e697372bb682dd3415d58032
Erik Michaels-Ober <sferik@gmail.com>
2013-04-08 16:43:03 -0700

    Add features

--- movies.rb
+++ movies.rb
@@ -21,19 +21,36 @@ class Movies < Sinatra::Base
 
   get '/search' do
     @query = params[:q]
+    @page_title += ": Search Results for #{@query}"
     @button = params[:button]
-    file = open("http://www.omdbapi.com/?s=#{URI.escape(@query)}")
-    @results = JSON.load(file.read)["Search"] || []
-    erb :results
+    if @button == "lucky"
+      file = open("http://www.omdbapi.com/?t=#{URI.escape(@query)}")
+      @result = JSON.load(file.read)
+      set_actors_and_directors(@result)
+      erb :detail
+    else
+      file = open("http://www.omdbapi.com/?s=#{URI.escape(@query)}")
+      @results = JSON.load(file.read)["Search"] || []
+      if @results.size == 1
+        redirect "/movies?id=#{@results.first["imdbID"]}"
+      else
+        erb :results
+      end
+    end
   end
 
   get '/movies' do
     @id = params[:id]
     file = open("http://www.omdbapi.com/?i=#{URI.escape(@id)}&tomatoes=true")
     @result = JSON.load(file.read)
-    @actors = @result["Actors"].split(", ")
-    @directors = @result["Director"].split(", ")
+    @page_title += ": #{@result["Title"]}"
+    set_actors_and_directors(@result)
     erb :detail
+  end
+
+  def set_actors_and_directors(result)
+    @actors = result["Actors"].split(", ")
+    @directors = result["Director"].split(", ")
   end
 
   run!
