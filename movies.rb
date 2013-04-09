660974735aad61bb23df7a3e827746d986f205e3
Erik Michaels-Ober <sferik@gmail.com>
2013-04-08 17:19:04 -0700

    Add related results to details view

--- movies.rb
+++ movies.rb
@@ -23,24 +23,12 @@ class Movies < Sinatra::Base
     @query = params[:q]
     @page_title += ": Search Results for #{@query}"
     @button = params[:button]
-    if @button == "lucky"
-      file = open("http://www.omdbapi.com/?t=#{URI.escape(@query)}")
-      @result = JSON.load(file.read)
-      if @result["Actors"] && @result["Directors"]
-        set_actors_and_directors(@result)
-        erb :detail
-      else
-        @results = []
-        erb :results
-      end
+    file = open("http://www.omdbapi.com/?s=#{URI.escape(@query)}")
+    @results = JSON.load(file.read)["Search"] || []
+    if @results.size == 1 || (@results.size > 1 && @button == "lucky")
+      redirect "/movies?id=#{@results.first["imdbID"]}"
     else
-      file = open("http://www.omdbapi.com/?s=#{URI.escape(@query)}")
-      @results = JSON.load(file.read)["Search"] || []
-      if @results.size == 1
-        redirect "/movies?id=#{@results.first["imdbID"]}"
-      else
-        erb :results
-      end
+      erb :serp
     end
   end
 
@@ -49,14 +37,13 @@ class Movies < Sinatra::Base
     @query = params[:q]
     file = open("http://www.omdbapi.com/?i=#{URI.escape(@id)}&tomatoes=true")
     @result = JSON.load(file.read)
+    file = open("http://www.omdbapi.com/?s=#{URI.escape(@result["Title"])}")
+    @results = JSON.load(file.read)["Search"] || []
+    @results.reject!{|movie| movie["Title"] == @result["Title"]}
     @page_title += ": #{@result["Title"]}"
-    set_actors_and_directors(@result)
+    @actors = @result["Actors"].split(", ")
+    @directors = @result["Director"].split(", ")
     erb :detail
-  end
-
-  def set_actors_and_directors(result)
-    @actors = result["Actors"].split(", ")
-    @directors = result["Director"].split(", ")
   end
 
   run!
