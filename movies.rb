61f7fbf82617db2ecf37571c426350c2bff76589
Erik Michaels-Ober <sferik@gmail.com>
2013-04-08 21:20:40 -0700

    Cleanup layout

--- movies.rb
+++ movies.rb
@@ -9,17 +9,20 @@ class Movies < Sinatra::Base
   end
 
   before do
+    @page = :default
     @app_name = "Movies App"
     # Sets the default page title
     @page_title = @app_name
   end
 
   get '/' do
+    @page = :home
     @page_title += ": Home"
-    erb :home
+    erb @page
   end
 
   get '/search' do
+    @page = :serp
     @query = params[:q]
     @page_title += ": Search Results for #{@query}"
     @button = params[:button]
@@ -28,22 +31,24 @@ class Movies < Sinatra::Base
     if @results.size == 1 || (@results.size > 1 && @button == "lucky")
       redirect "/movies?id=#{@results.first["imdbID"]}"
     else
-      erb :serp
+      erb @page
     end
   end
 
   get '/movies' do
+    @page = :movie
     @id = params[:id]
-    @query = params[:q]
+    @query = params[:q] || ""
     file = open("http://www.omdbapi.com/?i=#{URI.escape(@id)}&tomatoes=true")
     @result = JSON.load(file.read)
     file = open("http://www.omdbapi.com/?s=#{URI.escape(@result["Title"])}")
     @results = JSON.load(file.read)["Search"] || []
     @results.reject!{|movie| movie["Title"] == @result["Title"]}
     @page_title += ": #{@result["Title"]}"
+    @genres = @result["Genre"].split(", ")
     @actors = @result["Actors"].split(", ")
     @directors = @result["Director"].split(", ")
-    erb :detail
+    erb :movie
   end
 
   run!
